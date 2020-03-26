1140a6438294126a47eb8f2b3e4d29211198b983
dizzydoo <mikeallum2000@gmail.com>
2020-03-26 12:30:13 -0400

    oops i messed up the help.js

--- commands/help.js
+++ commands/help.js
@@ -12,20 +12,6 @@ module.exports = {
         const { commands } = message.client;
 
         if (!args.length) {
-			const name = args[0].toLowerCase();
-			const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
-
-			if (!command) {
-				return message.reply('that\'s not a valid command!');
-			}
-
-			data.push(`__**${command.name}**__`);
-
-			if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-			if (command.description) data.push(`**Description:** ${command.description}`);
-			if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
-
-			message.channel.send(data, { split: true });
 
             data.push('Here\'s a list of all my commands:');
             data.push(commands.map(command => command.name).join(', '));
@@ -40,7 +26,22 @@ module.exports = {
                     console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
                     message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
                 });
-        }
+		}
+		
+		const name = args[0].toLowerCase();
+		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+		if (!command) {
+			return message.reply('that\'s not a valid command!');
+		}
+
+		data.push(`__**${command.name}**__`);
+
+		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+		if (command.description) data.push(`**Description:** ${command.description}`);
+		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+
+		message.channel.send(data, { split: true });
 
 	},
 };
