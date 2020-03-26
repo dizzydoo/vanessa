a1d62bd1f619f58f37b53eb3eccd7aebd21827d4
dizzydoo <mikeallum2000@gmail.com>
2020-03-26 12:26:10 -0400

    update help.js for specific command info

--- commands/help.js
+++ commands/help.js
@@ -12,6 +12,21 @@ module.exports = {
         const { commands } = message.client;
 
         if (!args.length) {
+			const name = args[0].toLowerCase();
+			const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+			if (!command) {
+				return message.reply('that\'s not a valid command!');
+			}
+
+			data.push(`__**${command.name}**__`);
+
+			if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+			if (command.description) data.push(`**Description:** ${command.description}`);
+			if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+
+			message.channel.send(data, { split: true });
+
             data.push('Here\'s a list of all my commands:');
             data.push(commands.map(command => command.name).join(', '));
             data.push(`\nYou can send \`${botPrefix}help [command name]\` to get info on a specific command!`);
